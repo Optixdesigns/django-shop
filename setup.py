e7966240008288171cb18887903f42f82d46a03f
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-05 11:52:32 +0200

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import os
+
+setup(name='django-shop',
+    version='0.0.1',
+    description='Django Shop',
+    #long_description = read('README.rst'),
+    author='Sjoerd Arendsen',
+    url='https://github.com/Optixdesigns/django-shop',
+    #install_requires=('django-taggit', 'bleach', 'django-mptt', 'django-autoslug',),
+    packages=find_packages(exclude=["example", "example.*"]),
+    include_package_data=True,
+    zip_safe = False
+)
