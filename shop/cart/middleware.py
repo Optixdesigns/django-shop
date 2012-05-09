a3da8bf11dd8cbd267b23e7eddc22baec9f1843a
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-09 16:04:19 +0200

    Tem

--- shop/cart/middleware.py
+++ shop/cart/middleware.py
@@ -2,7 +2,7 @@ from django.utils.functional import SimpleLazyObject
 from shop.cart.models import Cart
 
 class CartMiddleware(object):
-    def process_request(self, request):
-        request.cart = SimpleLazyObject(
-            lambda: Cart.objects.get_for_request(request)
-        )
+  def process_request(self, request):
+    request.cart = SimpleLazyObject(
+        lambda: Cart.objects.get_for_request(request)
+    )
--- shop/cart/views.py
+++ shop/cart/views.py
@@ -3,5 +3,8 @@ from shop.cart.models import Cart
 from django.views.generic import (TemplateView, ListView, DetailView, View)
 from django.views.generic.base import TemplateResponseMixin
 
-class CartView(DetailView):
-    model = Cart
+class CartView(ListView):
+  template_name = "shop/cart.haml"
+
+  def get_queryset(self):
+    return self.request.cart.items.all()
